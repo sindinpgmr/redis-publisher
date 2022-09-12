9adeb40e75e3feda3cc860c71adf9afc59139f95
sindin <sindin.pgmr@gmail.com>
2022-09-12 18:31:27 +0430

    intial project

---
+++ src/main/java/com/example/redispublisher/Joke.java
@@ -0,0 +1,19 @@
+package com.example.redispublisher;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Joke implements Serializable {
+
+    private static final String JOKE_FORMAT = "Q: %s \nA: %s";
+
+    private String setup;
+    private String punchline;
+
+    @Override
+    public String toString() {
+        return String.format(JOKE_FORMAT, this.setup, this.punchline);
+    }
+}
---
+++ src/main/java/com/example/redispublisher/PublisherService.java
@@ -0,0 +1,43 @@
+package com.example.redispublisher;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.ReactiveRedisOperations;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class PublisherService {
+
+    private static final String JOKE_API_ENDPOINT = "https://joke.deno.dev/";
+    private WebClient webClient;
+
+    @Autowired
+    private ReactiveRedisOperations<String, Joke> redisTemplate;
+
+    @Value("${topic.name:joke-channel}")
+    private String topic;
+
+    @PostConstruct
+    private void init(){
+        this.webClient = WebClient.builder()
+                .baseUrl(JOKE_API_ENDPOINT)
+                .build();
+    }
+
+    @Scheduled(fixedRate = 3000)
+    public void publish(){
+        this.webClient.get()
+                .retrieve()
+                .bodyToMono(Joke.class)
+                .flatMap(joke -> this.redisTemplate.convertAndSend(topic, joke))
+                .subscribe();
+
+
+
+    }
+
+}
